cca1ed8722984b84c466d8d856ebeb90efaadac9
Stefanos Larkou <S.Larkou2@newcastle.ac.uk>
2021-02-12 14:36:00 +0200

    Add class District

---
+++ src/District.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class District {
+
+    private String name;
+    private ArrayList<Incident> incidentsInDistrict;
+
+    public District(String name) {
+        this.name = name;
+        this.incidentsInDistrict = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Incident> getIncidents() {
+        return incidentsInDistrict;
+    }
+
+    public void setIncidents(ArrayList<Incident> incidentsInDistrict) {
+        this.incidentsInDistrict = incidentsInDistrict;
+    }
+
+    public void addIncident(Incident i){
+        getIncidents().add(i);
+    }
+
+    public String toString(){
+        return "District name:\t" + getName() + "\n"
+                + "Incidents:\t\n" + getIncidents() + "\n";
+    }
+}
